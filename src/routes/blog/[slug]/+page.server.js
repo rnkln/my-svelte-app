5608e96c59b19c973e0e4e4ff2a027962362f5ed
Rune Klein <runeklein@outlook.com>
2024-08-05 23:29:27 +0200

    ✨ Prerender pages

--- src/routes/blog/[slug]/+page.server.js
+++ src/routes/blog/[slug]/+page.server.js
@@ -1,15 +1,24 @@
 import { error } from '@sveltejs/kit'
 import { posts } from '../../../data/blog'
 
+export const prerender = true
+
 /** @type {import('./$types').PageServerLoad} */
 export function load({ params }) {
 	const post = posts.find((post) => post.slug === params.slug)
 
-	if (post === undefined) {
-		throw error(404)
+	if (post !== undefined) {
+		return {
+			post
+		}
 	}
 
-	return {
-		post
-	}
+	error(404)
+}
+
+/** @type {import('./$types').EntryGenerator} */
+export function entries() {
+	return posts.map((post) => ({
+		slug: post.slug
+	}))
 }
